e184d712566d28e30244f48a56f04a0e16bc08c7
madhumithadasarathy <madhumithadasarathy@gmail.com>
2026-02-04 18:32:00 +0530

    Sponsors

--- src/pages/Sponsors.jsx
+++ src/pages/Sponsors.jsx
@@ -1,25 +1,11 @@
-import React from "react";
+import React from 'react'
 
 function Sponsors() {
   return (
-    <section className="relative w-full h-screen overflow-hidden">
-      {/* Background Video */}
-      <video
-        className="absolute top-0 left-0 w-full h-full object-cover"
-        src="/broke.mp4"
-        autoPlay
-        loop
-        muted
-        playsInline
-      />
-
-      {/* Optional overlay (remove if you don't want darkening) */}
-      <div className="absolute inset-0 bg-black/40" />
-
-      {/* Content Layer */}
-
-    </section>
-  );
+    <div>
+      Sponsors
+    </div>
+  )
 }
 
-export default Sponsors;
+export default Sponsors
